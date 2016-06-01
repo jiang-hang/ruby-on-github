c2957e8249cb5cdced7e7003f938b98143de0d41
jiang-hang <jianghang@bagualu.net>
2016-06-01 14:27:48 +0800

    rename it to ruby on github

--- 1a-search.r
+++ 1a-search.r
@@ -5,7 +5,7 @@ source("requests.r")
 search_repo <- function(query, page = NULL) {
   Sys.sleep(12)
   path <- paste0(base, "/search/repositories")
-  qs <- list(q = paste("language:r", query), per_page = 100, page = page)
+  qs <- list(q = paste("stars:>100","language:ruby", query), per_page = 100, page = page)
   
   req <- GET(path, config, query = qs)
   c <- content(req)
@@ -32,23 +32,33 @@ search_repo <- function(query, page = NULL) {
   
   # Combine individual pages
   max_page <- ceiling(n / 100)
+  if( max_page > 10 ){ 
+	max_page = 10
+  }
   results <- lapply(2:max_page, search_repo, query = query)
   c(items, unlist(results, recursive = FALSE))
 }
 
 if (!file.exists("cache/prehistory.rds")) {
-  prehistory <- search_repo("created:<2011")
+  prehistory <- search_repo("created:<2008-01-01")
   saveRDS(prehistory, "cache/prehistory.rds") 
 } else {
   prehistory <- readRDS("cache/prehistory.rds")
 }
 
-get_month <- function(year, month) {
+get_month <- function(year, month, day) {
   cache_path <- paste0("cache/", year, "-", month, ".rds")
   if (file.exists(cache_path)) return(readRDS(cache_path))
   
   message("Downloading repos created in ", year, "-", month)
-  query <- paste0("created:", year, "-", month - 1, "..", year, "-", month)
+  if( month != 12 ) {
+      start_s=sprintf("%4d-%02d-%02d",year,month,1)
+      end_s=sprintf("%4d-%02d-%02d",year,month+1,1)
+  }else{
+      start_s=sprintf("%4d-%02d-%02d",year,month,1)
+      end_s=sprintf("%4d-%02d-%02d",year,month,31)
+  }
+  query <- paste0("created:", start_s,"..", end_s)
   results <- search_repo(query)
   
   saveRDS(results, cache_path)
@@ -58,8 +68,8 @@ get_month <- function(year, month) {
 all_months <- function() {
   cur_year <- year(today())
   
-  past <- expand.grid(year = 2011:(cur_year - 1), month = 1:12)
-  pres <- data.frame(year = cur_year, month = 1:(month(today()) - 1))
+  past <- expand.grid(year = 2008:(cur_year - 1), month = 1:12, day=1)
+  pres <- data.frame(year = cur_year, month = 1:(month(today()) - 1), day=1)
   all <- rbind(past, pres)
   
   unlist(mlply(all, get_month), recursive = FALSE)
